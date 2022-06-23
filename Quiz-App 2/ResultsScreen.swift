9684af18e30648c1ff93fdf69dc5f92f9d0e399d
P0lterg3ist <87923089+P0lterg3ist@users.noreply.github.com>
2022-06-23 16:07:07 +0800

    Update ResultsScreen.swift
    Added support for Dark Mode in .sheet

--- Quiz-App 2/ResultsScreen.swift
+++ Quiz-App 2/ResultsScreen.swift
@@ -4,20 +4,30 @@ struct ResultsScreen: View {
     
     var score: Int
     var totalQuestions: Int
+    var colour: Color
+    var colourNegate: Color
     
     var body: some View {
-        VStack {
-            CircularProgressView(progress: CGFloat(score) / CGFloat(totalQuestions))
-                .frame(width: 150, height: 150)
-            
-            Text("You got")
-            Text("\(score)/\(totalQuestions)")
+        ZStack {
+            Rectangle()
+                .background(colour)
+                .foregroundColor(colour)
+            VStack {
+                CircularProgressView(progress: CGFloat(score) / CGFloat(totalQuestions))
+                    .frame(width: 150, height: 150)
+                
+                Text("You got")
+                    .padding()
+                    .foregroundColor(colourNegate)
+                Text("\(score) / \(totalQuestions) correct!")
+                    .foregroundColor(colourNegate)
+            }
         }
     }
 }
 
 struct ResultsScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ResultsScreen(score: 3, totalQuestions: 6)
+        ResultsScreen(score: 3, totalQuestions: 6, colour: Color.white, colourNegate: Color.black)
     }
 }
